66cce69c207a16f907678218229bb20d41333db7
Pablo Cirant <cirant3002@gmail.com>
2020-05-27 16:29:30 -0400

    grid sistem

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/custom.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div className="row">
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+        <div className="col-3 col-sm-1">
+          <div className="fill"></div>
+        </div>
+      </div>
     </div>
   );
 }
